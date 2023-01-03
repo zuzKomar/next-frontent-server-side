2e77bd6b06363614b2c3c65400226ed529ec6e8f
Zuzanna Komar <s17301@pjwstk.edu.pl>
2023-01-03 21:03:38 +0100

    Added 404 not found page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import { PageContainer } from "./components/PageContainer";
+
+function NotFoundPage(){
+    return(
+        <PageContainer>
+            <h1>Sorry, page not found!</h1>
+        </PageContainer>
+    )
+}
+
+export default NotFoundPage;
