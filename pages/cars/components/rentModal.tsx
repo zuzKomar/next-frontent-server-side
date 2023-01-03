9f9d40ae9526bb971ddc65ba81ed452826a6ade1
Zuzanna Komar <s17301@pjwstk.edu.pl>
2023-01-03 21:07:11 +0100

    Added rent a car modal

---
+++ pages/cars/components/rentModal.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import {AlertDialog, Flex} from '@adobe/react-spectrum'
+import {DatePicker} from '@adobe/react-spectrum'
+import {parseDate} from '@internationalized/date';
+
+type RentModalProps = {
+ carId: number;
+ costPerDay: number;
+ closeHandler: (open: boolean) => void
+ confirmHandler: (carId: number, userId: number, date: string, dueDate: string) => void;
+}
+
+export const days = (date_1: Date, date_2: Date) => {
+    let difference = date_1.getTime() - date_2.getTime();
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+}
+
+
+const RentModal = ({carId, costPerDay, closeHandler, confirmHandler}: RentModalProps) => {
+    const [date, setDate] = useState<string | any>(parseDate(new Date().toISOString().split('T')[0]));
+    const [dueDate, setDueDate] = useState<string | any>(parseDate(new Date().toISOString().split('T')[0]));
+
+    let costOfRent = costPerDay * days(new Date(dueDate), new Date(date));
+
+    return (
+        <AlertDialog 
+            title="Pick rent time"
+            primaryActionLabel="Rent"
+            cancelLabel="Cancel"
+            variant="confirmation"
+            onCancel={() => closeHandler(false)}
+            onPrimaryAction={() => confirmHandler(carId, 1, date, dueDate)}
+        >
+            Select a suitable dates for rent period. Note that the minimal rent time is one full day (24h).
+            <Flex direction="row" gap="size-200">
+                <DatePicker label="Date from:" value={date} onChange={setDate}/>
+                <DatePicker label="Date to:" value={dueDate} onChange={setDueDate}/>
+            </Flex>
+            { costOfRent > 0 &&
+                <Flex direction='row' gap="size-100">
+                    <p>Cost of rent:</p>
+                    <p>{costOfRent + ' $'}</p>
+                </Flex>
+            }
+        </AlertDialog>
+    )
+}
+
+export default RentModal;
