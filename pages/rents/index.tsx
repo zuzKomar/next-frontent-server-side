d1d033eab7d4e9c5b5de083fc73aaba76ac5d42b
Zuzanna Komar <s17301@pjwstk.edu.pl>
2023-01-04 17:16:37 +0100

    Enabled car damage reporting

--- pages/rents/index.tsx
+++ pages/rents/index.tsx
@@ -1,15 +1,18 @@
 import { PageContainer } from '../components/PageContainer';
-import { Cell, Column, Row, TableView, TableBody, TableHeader } from '@adobe/react-spectrum';
+import { Cell, Column, Row, TableView, TableBody, TableHeader, Button } from '@adobe/react-spectrum';
 import { GetServerSideProps } from 'next';
 import { days } from '../cars/components/rentModal';
+import { Car } from '../cars';
+import { useRouter } from 'next/router'
 
 export type Rent = {
   id: number;
   userId: number;
   carId: number;
-  date: Date
-  dueDate: Date
-  damagedCar: boolean
+  date: Date;
+  dueDate: Date;
+  damagedCar: boolean;
+  car: Car;
 }
 
 type RentsPageProps = {
@@ -17,20 +20,45 @@ type RentsPageProps = {
 }
 
 export default function Rents({rents}: RentsPageProps) {
+  const router = useRouter();
 
   let columns = [
+    { name: 'Car', uid: 'car'},
     { name: 'Date from', uid: 'date' },
     { name: 'Date to', uid: 'dueDate' },
     { name: 'Cost', uid: 'cost' },
-    { name: 'Damage', uid: 'damagedCar' }
+    { name: 'Damage', uid: 'damagedCar' },
+    { name: 'Options', uid: 'reportDamage'}
   ];
 
   let modifiedRents = rents.map((rent) =>  (
     {
       ...rent,
-      cost: days(new Date(rent.dueDate), new Date(rent.date))
+      date: new Date(rent.date).toISOString().slice(0,10),
+      dueDate: new Date(rent.dueDate).toISOString().slice(0,10),
+      car: rent.car.brand + rent.car.model,
+      damagedCar : rent.car.usable ? 'No' : 'Yes',
+      cost: days(new Date(rent.dueDate), new Date(rent.date)) * rent.car.costPerDay,
+      reportDamage: 'yes'
     }
     ));
+
+  function handleDamageReport( carId: number){
+
+    let updateCarDto = {
+      usable: false
+    }
+    fetch(`http://localhost:3000/cars/${carId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updateCarDto),
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(() => router.push(`/rents`))
+    .catch((e) => console.log(e))
+  }
 
   return (
     <PageContainer>
@@ -41,8 +69,7 @@ export default function Rents({rents}: RentsPageProps) {
           selectionMode="single"
           selectionStyle="highlight"
           alignSelf="center"
-          width="70%"
-          onSelectionChange={()=>console.log('test')}
+          width="60%"
         >
           <TableHeader columns={columns}>
             {column => (
@@ -52,7 +79,10 @@ export default function Rents({rents}: RentsPageProps) {
             )}
           </TableHeader>
           <TableBody items={modifiedRents}>
-            {(item: any) => <Row>{columnKey => <Cell>{item[columnKey]}</Cell>}</Row>}
+            {(item: any) => 
+            <Row>{columnKey => <Cell>{ columnKey !== 'reportDamage' ? item[columnKey] : <Button variant='primary' isDisabled={item.damagedCar === 'No' ? false : true} onPress={() => handleDamageReport(item.carId)}>Report damage</Button>}</Cell>}
+            </Row>
+            }
           </TableBody>
         </TableView>
     </PageContainer>
