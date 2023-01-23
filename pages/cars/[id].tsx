314936e4e8662742f468dfa8aa47a3b7461bedc8
Zuzanna Komar <s17301@pjwstk.edu.pl>
2023-01-23 16:36:47 +0100

    Fixed selected car page

--- pages/cars/[id].tsx
+++ pages/cars/[id].tsx
@@ -1,13 +1,15 @@
 import { PageContainer } from '../components/PageContainer';
-import { GetServerSideProps } from 'next';
+import { GetServerSidePropsContext } from 'next';
 import { Car } from '.';
 import SelectedCarInfo from './components/selectedCarInfo';
+import { getSession } from 'next-auth/react';
+import { Session } from 'next-auth';
 
 export interface CarPageProps {
     data: Car
 }
 
-function CarPage({data}: CarPageProps){
+export default function CarPage({data}: CarPageProps){
 
     return(
         <PageContainer>
@@ -16,10 +18,42 @@ function CarPage({data}: CarPageProps){
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch(`http://localhost:3000/cars/${context.params!.id}`);
-  const data = await res.json();
-  return { props: { data } };
+export async function getServerSideProps (context: GetServerSidePropsContext){
+    try {
+        const session: Session | null = await getSession({req: context.req});
+
+        if(!session) {
+            return {
+              redirect: {
+                destination: '/auth/signin',
+                permanent: false
+              }
+            }
+          }else {
+            const token = session!.user!.accessToken || '';
+            const car = await getCar(context.params!.id, token!);
+          
+            return { 
+              props: { 
+                data: car
+              } 
+            }
+          }
+
+    }catch(err){
+        console.log(err);
+    }
 };
 
-export default CarPage
+export async function getCar(carId: string, token: string) {
+    const response = await fetch(`${process.env.NEST_URL}cars/${carId}`, 
+    {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    });
+    const data = await response.json();
+    return data;
+  }
