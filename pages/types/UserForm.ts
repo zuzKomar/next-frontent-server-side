2e6344caf440c87b68d19807bb5e41b9fc2a3af7
Zuzanna Komar <s17301@pjwstk.edu.pl>
2023-02-06 21:03:23 +0100

    Added type objects

---
+++ pages/types/UserForm.ts
@@ -0,0 +1,9 @@
+export type IFormInputs = {
+    firstName?: string
+    lastName?: string
+    phone?: string
+    pesel?: string
+    email?: string
+    password?: string
+    confirmPassword?: string
+  };  
